17a01997484a41c9b29ff7aac83d65fe0b304d93
jinbe- <matsubara0501jp@gmail.com>
2025-08-31 22:42:34 +0900

    commit lesson code

---
+++ BlazorSampleApp/BlazorSampleApp/Components/Objects/MeasureService.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorSampleApp.Components.Objects
+{
+    public static class MeasureService
+    {
+        private static readonly Random _random = new Random();
+
+        public static MeasureResult Measure()
+        {
+            return new MeasureResult
+            {
+                Value = _random.NextDouble(),
+                Date = DateTime.Now
+            };
+        }
+
+    }
+
+    public class MeasureResult
+    {
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
