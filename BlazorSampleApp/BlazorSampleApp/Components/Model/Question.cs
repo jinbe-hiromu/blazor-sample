6774ab450de11120b4c319263adddbc9fd1ee2fc
jinbe- <matsubara0501jp@gmail.com>
2025-09-03 22:11:45 +0900

    Add FactoryClass

--- BlazorSampleApp/BlazorSampleApp/Components/Model/Question.cs
+++ BlazorSampleApp/BlazorSampleApp/Components/Model/Question.cs
@@ -1,15 +1,27 @@
 ﻿namespace BlazorSampleApp.Components.Model
 {
-    // モデル
     public enum QuestionType { Choice, Text }
 
-    public class Question
+    // 共通部分
+    public abstract class Question
     {
-        public string Text { get; set; } = "";
+        public string Text { get; set; } = string.Empty;
         public int Score { get; set; } = 10;
-        public QuestionType Type { get; set; } = QuestionType.Text;
+        public abstract QuestionType Type { get; }
+    }
+
+    // 選択式
+    public class ChoiceQuestion : Question
+    {
+        public override QuestionType Type => QuestionType.Choice;
         public List<string> Choices { get; set; } = new();
-        public int CorrectIndex { get; set; } = -1; // 選択式の正解インデックス
-        public string? CorrectText { get; set; }    // 記述式の正解
+        public int CorrectIndex { get; set; } = -1;
+    }
+
+    // 記述式
+    public class TextQuestion : Question
+    {
+        public override QuestionType Type => QuestionType.Text;
+        public string CorrectText { get; set; } = string.Empty;
     }
 }
---
+++ BlazorSampleApp/BlazorSampleApp/Components/Objects/QuestionFactory.cs
@@ -0,0 +1,34 @@
+﻿using BlazorSampleApp.Components.Model;
+
+namespace BlazorSampleApp.Components.Objects
+{
+
+    public interface IQuestionFactory
+    {
+        Question Create(string questionText, int score);
+    }
+
+    public class ChoiceQuestionFactory : IQuestionFactory
+    {
+        public Question Create(string questionText, int score)
+        {
+            return new ChoiceQuestion
+            {
+                Text = questionText,
+                Score = score
+            };
+        }
+    }
+
+    public class TextQuestionFactory : IQuestionFactory
+    {
+        public Question Create(string questionText, int score)
+        {
+            return new TextQuestion
+            {
+                Text = questionText,
+                Score = score
+            };
+        }
+    }
+}
